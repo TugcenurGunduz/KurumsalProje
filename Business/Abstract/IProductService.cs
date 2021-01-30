6eb33aea34c97c14f892f2d437c46198f8fb8f99
TugcenurGunduz <tnurgunduz@hotmail.com>
2021-01-31 00:09:47 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll(); //tüm ürünleri listeleyecek bir ortam oluştu
+    }
+}
