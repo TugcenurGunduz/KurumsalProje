a22174262ab1d49a12dfe2d27fccf8c576287763
TugcenurGunduz <tnurgunduz@hotmail.com>
2021-02-03 23:55:56 +0300

    Entity Framework Veritabanı Bağlantısı

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -8,5 +8,7 @@ namespace Business.Abstract
     public interface IProductService
     {
         List<Product> GetAll(); //tüm ürünleri listeleyecek bir ortam oluştu
+        List<Product> GetAllByCategoryId(int id);
+        List<Product> GetByUnitPrice(decimal min, decimal max);
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.InMemory
@@ -29,7 +30,7 @@ namespace DataAccess.Concrete.InMemory
         public void Delete(Product product)
         {
             //LINQ - Language Integrated Query (Dile Gömülü Sorgulama)
-            //lambda
+            //lambda =>
 
             //Product productToDelete = null;
             //foreach (var p in _products)
@@ -62,6 +63,16 @@ namespace DataAccess.Concrete.InMemory
         public List<Product> GetAllByCategory(int categoryId)
         {
            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
         }
     }
 }
