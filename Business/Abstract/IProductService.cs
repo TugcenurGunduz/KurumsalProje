4783328d670e76c5f2b14426ba4308849b73b80a
TugcenurGunduz <tnurgunduz@hotmail.com>
2021-02-15 19:44:49 +0300

    Web API

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
 using Entities.DTOs;
 using System;
 using System.Collections.Generic;
@@ -8,9 +9,11 @@ namespace Business.Abstract
 {
     public interface IProductService
     {
-        List<Product> GetAll(); //tüm ürünleri listeleyecek bir ortam oluştu
-        List<Product> GetAllByCategoryId(int id);
-        List<Product> GetByUnitPrice(decimal min, decimal max);
-        List<ProductDetailDto> GetProductDetails();
+        IDataResult<List<Product>> GetAll(); //tüm ürünleri listeleyecek bir ortam oluştu
+        IDataResult<List<Product>> GetAllByCategoryId(int id);
+        IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+        IDataResult<Product> GetById(int productId); //bir ürün döndürür
+        IResult Add(Product product);
     }
 }
