58d78aebab78f2e6734c9cf6a3f417c3f1f92bfd
TugcenurGunduz <tnurgunduz@hotmail.com>
2021-02-16 17:40:09 +0300

    Web API

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -38,10 +38,11 @@ namespace Business.Concrete
         {
             //İş kodları
             //Yetkisi var mı?
-            if(DateTime.Now.Hour==22)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
+
+            //if (DateTime.Now.Hour == 16)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            //}
 
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
             
